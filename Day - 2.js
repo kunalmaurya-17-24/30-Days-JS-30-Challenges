1774c0ffbda9aba96d4a1f40ed81faf0383385a5
kunalmaurya-17-24 <166983031+kunalmaurya-17-24@users.noreply.github.com>
2024-07-16 14:46:55 +0530

    Day 2

---
+++ Day - 2.js
@@ -0,0 +1,69 @@
+//Activity 1
+
+//Task 1
+var a = 5;
+var b = 7;
+
+var sum = (a+b);
+console.log(sum);
+
+
+//Task 2
+
+var diff = (a-b);
+console.log(diff);
+
+//Task 3
+var mul = (a*b);
+console.log(mul);
+
+//Task 4
+var divide = a/b;
+console.log(divide);
+
+//Task 5
+var reminder = a%b;
+console.log(reminder);
+
+//Activity 2
+
+//Task 6
+a+=9;
+console.log(a);
+
+//Task 7
+b-=3;
+console.log(b);
+
+//Activity 3
+
+//Task 8
+console.log(a<b);
+console.log(a>b);
+
+//Task 9
+var e = 623;
+var f = 24124;
+console.log(f>=e);
+console.log(e>=f);
+
+//Task 10
+console.log(a==b);
+console.log(a===b);
+
+
+//Activity 4
+
+//Task 11
+console.log(f>e && f>a);
+
+//Tak 12
+console.log(a>b || b>a);
+
+//Task 13
+console.log(!(e>f));
+
+//Activity 5
+
+//Task 14
+console.log( a>0 ? true : false);
