2bacc2321ba218e13de351445a196ea721411bce
kunalmaurya-17-24 <166983031+kunalmaurya-17-24@users.noreply.github.com>
2024-07-16 14:46:22 +0530

    Day-1

---
+++ Day1.js
@@ -0,0 +1,43 @@
+//Activity 1
+var me = 95 //Task 1
+console.log(me)
+
+let you = "sparkel_god" 
+console.log(you) //Task 2
+
+//Activity 2
+const isBool = true //Task 3
+console.log(isBool)
+
+//Activity 3 // Task 4
+let num = 95;
+console.log(typeof(num))
+
+let str = "SPIDERMAN"
+console.log(typeof(str))
+
+let isTrue = false
+console.log(typeof(isTrue))
+
+let arr = [5,2,4,5,6,7]
+console.log(typeof(arr))
+
+let obje = {
+    username : "sparkel_god",
+    password : "***********"
+}
+
+console.log(typeof(obje))
+
+
+//Activity 4 //Task 5
+let newVar = 56
+console.log(newVar)
+newVar = 95;
+console.log(newVar)
+
+//Acticity 5 //Task 6
+
+const old = 95
+old = 53
+console.log(old)
